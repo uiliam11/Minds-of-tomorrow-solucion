774f9dbbf6773ac64835d8fe7a3f0faf6836bc81
uiliam11 <100007488.joan23@fje.edu>
2020-11-26 12:10:30 +0100

    subida 26/11

--- js/codigo.js
+++ js/codigo.js
@@ -5,10 +5,13 @@ window.onscroll = function() {
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
         document.getElementById("nav").style.backgroundColor = "rgba(255,255,255,0.5)";
-        document.getElementById("nav").style.backdropFilter = "blur(4px)";
+        document.getElementById("nav").style.backdropFilter = "blur(8px)";
+        //document.getElementById("nav").style.backdropFilter = "invert(1)";
+        //document.getElementById("nav").style.backdropFilter = "sepia(0.9)";
     } else {
         document.getElementById("nav").style.backgroundColor = "rgba(0,0,0,0.0)";
         document.getElementById("nav").style.backdropFilter = "blur(0px)";
-
+        //document.getElementById("nav").style.backdropFilter = "invert(0)";
+        //document.getElementById("nav").style.backdropFilter = "sepia(0)";
     }
 }
